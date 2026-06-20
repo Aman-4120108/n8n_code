7214815e0cbc3ecf4933073f08324daae857aa7e
Aman-4120108 <aman.raj.sde02@gmail.com>
2026-06-21 01:42:09 +0530

    added code

--- scripts/test.py
+++ scripts/test.py
@@ -1,10 +1,6 @@
 import json
-from datetime import datetime
 
-result = {
+print(json.dumps({
     "status": "success",
-    "message": "Hello from Python",
-    "timestamp": datetime.now().isoformat()
-}
-
-print(json.dumps(result))
+    "message": "Hello from Python"
+}))
